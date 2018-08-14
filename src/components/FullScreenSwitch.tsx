b372753d8bb79d2cb07c6eaec2e5ffeae9f7106c
forest93 <forest93@163.com>
2018-08-14 21:32:12 +0800

    Improve the compatibility with IE 11. We haven't done with it yet.

--- src/components/FullScreenSwitch.tsx
+++ src/components/FullScreenSwitch.tsx
@@ -12,24 +12,41 @@ export class FullscreenSwitch extends React.Component<{}, IFullscreenSwitchState
         super(props);
         this.state = { isFullscreen: false };
     }
+    public componentWillMount() {
+        document.addEventListener("fullscreenchange", this.onFullScreenChanged);
+        document.addEventListener("webkitfullscreenchange", this.onFullScreenChanged);
+        document.addEventListener("MSFullscreenChange", this.onFullScreenChanged);
+    }
+    public componentWillUnmount() {
+        document.removeEventListener("fullscreenchange", this.onFullScreenChanged);
+        document.removeEventListener("webkitfullscreenchange", this.onFullScreenChanged);
+        document.removeEventListener("MSFullscreenChange", this.onFullScreenChanged);
+    }
     public render() {
         const onButtonClicked = (e: React.MouseEvent<HTMLElement>) => {
             if (this.state.isFullscreen) {
-                (document.exitFullscreen || document.webkitExitFullscreen).apply(document);
+                (document.exitFullscreen
+                    || document.webkitExitFullscreen
+                    || (document as any).msExitFullscreen).apply(document);
             } else {
-                (document.documentElement.requestFullscreen || document.documentElement.webkitRequestFullscreen).apply(document.documentElement);
+                (document.documentElement.requestFullscreen
+                    || document.documentElement.webkitRequestFullscreen
+                    || (document.documentElement as any).msRequestFullscreen).apply(document.documentElement);
             }
-            this.setState({ isFullscreen: !this.state.isFullscreen });
         };
         const isFullscreen = this.state.isFullscreen;
         return <Tooltip title={isFullscreen ? "Exit fullscreen" : "Fullscreen"}>
             <IconButton
                 onClick={onButtonClicked}
                 color="inherit"
-                disabled={!(document.fullscreenEnabled || document.webkitFullscreenEnabled)}
+                disabled={!(document.fullscreenEnabled || document.webkitFullscreenEnabled || (document as any).msFullscreenEnabled)}
             >
                 {isFullscreen ? <FullscreenExit /> : <Fullscreen />}
             </IconButton>
         </Tooltip>
     }
+    private onFullScreenChanged = (e: Event) => {
+        const isFullScreen = document.webkitIsFullScreen || !!(document as any).msFullscreenElement;
+        this.setState({ isFullscreen: isFullScreen });
+    };
 }
